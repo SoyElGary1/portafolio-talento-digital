4ad8f3184f28c47fcfc286e7625b429c8abeb571
Christopher Nunez <elgary1129@gmail.com>
2024-08-27 18:25:47 -0400

    Subiendo el proyecto

---
+++ assets/js/cards.js
@@ -0,0 +1,17 @@
+const cards = [
+    {
+        cardText: 'Encriptador de texto desarrollado con HTML, CSS y JS',
+        img: 'https://picsum.photos/700/400?random',
+        href:'',
+    },
+    {
+        cardText: 'Encriptador de texto desarrollado con HTML, CSS y JS',
+        img: 'https://picsum.photos/700/400?random',
+        href:'',
+    },
+    {
+        cardText: 'Encriptador de texto desarrollado con HTML, CSS y JS',
+        img: 'https://picsum.photos/700/400?random',
+        href:'',
+    }
+]
